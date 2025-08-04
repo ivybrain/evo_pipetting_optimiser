7068992cd141c4eb48b5a5913ebb807361ba2a73
ivybrain <ivy.brain@csl.com.au>
2025-08-04 11:43:50 +1000

    Removes cookiecutter test

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,11 +1,7 @@
-'''
+"""
 Execute by calling
 pytest
 in the command line from the root directory.
-'''
-import evo_pipetting_optimiser
+"""
 
-# This is sample code. Feel free to delete any of it
-def test_rect_area():
-    '''Tests the rect area function'''
-    assert evo_pipetting_optimiser.rect_area(3, 5) == 15
+import evo_pipetting_optimiser
