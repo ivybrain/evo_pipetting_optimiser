875417d040668bd2b68ea80b95605c1d9cb61964
ivybrain <ivy.brain@csl.com.au>
2026-05-06 13:49:34 +1000

    Moves previous dev to main for rework pr

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = parse_requirements("requirements.txt")
 
 setup(
     name="evo_pipetting_optimiser",
-    version="0.1.0a8",
+    version="0.1.0",
     packages=find_packages(
         include=["evo_pipetting_optimiser", "evo_pipetting_optimiser.*"]
     ),
